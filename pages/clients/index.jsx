256a2e367164e73bb51b4e0ab20b64e0e9f1bfc1
Muhammad Inam Ur Rehman <inaamurrehman343@gmail.com>
2023-05-16 14:30:04 +0500

    Add Running vibes

--- pages/clients/index.jsx
+++ pages/clients/index.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import styles from './clients.module.scss'
+import React from "react";
+import styles from "./clients.module.scss";
 
-import Image from 'next/image'
-import Heet from '../../images/heet.png'
-import Kee from '../../images/kee.png'
-import Link from 'next/link'
-import Head from 'next/head'
+import Image from "next/image";
+import Heet from "../../images/heet.png";
+import Kee from "../../images/kee.png";
+import Vibes from "../../images/vibes-2.png";
+import Link from "next/link";
+import Head from "next/head";
 
 const Clients = () => {
   return (
     <>
       <Head>
         <title>Our Clients | T & T Marketing </title>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/logo.png' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <div className={styles.clientInfo}>
         <div>
-          <Image src={Heet} alt='Heet Logo' />
+          <Image src={Heet} alt="Heet Logo" />
           <p>
             HEET Gaming is a premier gaming community that is dedicated to
             providing an exciting experience for gamers. HEET competes in
@@ -25,13 +26,13 @@ const Clients = () => {
             world-wide in the game of Counter-Strike. Members are known for
             their competitive spirit and love of the game.
           </p>
-          <Link className={`${styles.btn} button`} href='/clients/heet'>
+          <Link className={`${styles.btn} button`} href="/clients/heet">
             Case Study
           </Link>
         </div>
 
         <div>
-          <Image width='100%' src={Kee} alt='Keenetic Logo' />
+          <Image width="100%" src={Kee} alt="Keenetic Logo" />
           <p>
             Keenetic is an ambitious and innovative designer, developer &
             vendor, focused on Wi-Fi routers. They deliver commercially useful
@@ -39,13 +40,39 @@ const Clients = () => {
             models have been launched, and the number of users of the brand has
             exceeded seven million cross Europe.
           </p>
-          <Link className={`${styles.btn} button`} href='/clients/keenetic'>
+          <Link className={`${styles.btn} button`} href="/clients/keenetic">
+            Case Study
+          </Link>
+        </div>
+      </div>
+      <div className={styles.clientInfoSecond}>
+        <div>
+          <Image
+            src={Vibes}
+            alt="Vibes Logo"
+            style={{
+              filter: "revert",
+            }}
+          />
+          <p>
+            Running Vibes is a Website for runners by runners.We believe fitness
+            should be accessible to everyone, everywhere, regardless of income
+            or access to a gym. With hundreds of professional workouts, healthy
+            recipes and informative articles, as well as one of the most
+            positive communities on the web, you’ll have everything you need to
+            reach your personal fitness goals – for free!
+          </p>
+          <Link
+            className={`${styles.btn} button`}
+            target="_blank"
+            href="https://running-vibes.vercel.app/"
+          >
             Case Study
           </Link>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Clients
+export default Clients;
