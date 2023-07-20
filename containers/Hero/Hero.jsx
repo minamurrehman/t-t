21684c1a80660282da268361516924c55a2cc07e
Muhammad Inam Ur Rehman <inaamurrehman343@gmail.com>
2023-07-20 14:04:22 +0500

    Updated video

--- containers/Hero/Hero.jsx
+++ containers/Hero/Hero.jsx
@@ -1,18 +1,64 @@
-import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Hero.module.scss";
-import HeroImage from "../../images/hero.png";
+
+import { BiPause, BiPlay } from "react-icons/bi";
+import { VscUnmute, VscMute } from "react-icons/vsc";
 
 const Hero = () => {
+  const videoRef = useRef();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMute, setIsMute] = useState(true);
+
+  const videoHandler = (control) => {
+    if (control === "play") {
+      videoRef.current.play();
+      setIsPlaying(true);
+    } else if (control === "pause") {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    } else if (control === "mute") {
+      videoRef.current.muted = true;
+      setIsMute(true);
+    } else if (control === "unmute") {
+      videoRef.current.muted = false;
+      setIsMute(false);
+    }
+  };
   return (
     <div className={styles.container}>
-      <Image
-        src={HeroImage}
-        alt="Hero image we are marketing agency"
-        width={1400}
-        height={600}
-        className={styles.heroImage}
+      <video
+        src="/video.mp4"
+        className={styles.heroVideo}
+        poster="/hero.png"
+        muted
+        ref={videoRef}
       />
+      {!isPlaying ? (
+        <BiPlay
+          size={80}
+          onClick={() => videoHandler("play")}
+          className={styles.playButton}
+        />
+      ) : (
+        <BiPause
+          size={80}
+          onClick={() => videoHandler("pause")}
+          className={styles.playButton}
+        />
+      )}
+      {isMute ? (
+        <VscMute
+          size={24}
+          onClick={() => videoHandler("unmute")}
+          className={styles.muteButton}
+        />
+      ) : (
+        <VscUnmute
+          size={24}
+          onClick={() => videoHandler("mute")}
+          className={styles.muteButton}
+        />
+      )}
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import styles from "../styles/home.module.scss"
 import Carousel from '../components/Slider/Carousel'
 import Hear from '../components/Hear/Hear'
 
+
+
 export default function Home() {
   return (
     <>
@@ -16,70 +18,70 @@ export default function Home() {
           content="Boosting Your Brand's Reach and ROI with TT Marketing's Expert Strategies"
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/logo.png' />
+        <link rel='icon' href='/logo-black.svg' />
       </Head>
       <main>
         <Hero />
         <div className={styles.typeContainer}>
-          <Type/>
+          <Type />
           <div className={styles.typeText}>
-          <p>
-            Welcome to the <span className="text-secondary">T&T family</span>,
-            where marketing becomes a thrilling adventure. We are a bunch of
-            marketing geeks, fueled by the fire of our passion and a knack for
-            creativity. {" "}
-            <span className="text-secondary">
-              We don't just do our jobs, we live them.
-            </span>
-          </p>
-          <p>
-            Here at <span className="text-secondary">T&T</span>, we strive to
-            truly "get" your business. We dig deep, getting to grips with your
-            goals, your dreams, and even your worries.{" "}
-            <span className="text-secondary">
-              We're not just pushing out services, we're creating custom
-              solutions that mirror what your brand is all about.
-            </span>
-          </p>
-          <p>
-            Our bond with clients? Well, that's special. It's{" "}
-            <span className="text-secondary">
-              more than just a business deal
-            </span>
-            {" "}to us. We care for these relationships just as we do for our own
-            work - with heaps of dedication and warmth. We cheer when you
-            succeed and stand by your side when things get a bit rocky, {" "}
-            <span className="text-secondary">
-              always ready with a helping hand and a winning mindset
-            </span>
-            .
-          </p>
-          <p>
-            So, step in to a world of {" "}
-            <span className="text-secondary">T&T agency</span>, a place where
-            work meets passion, and {" "}
-            <span className="text-secondary">clients become friends</span>.
-            Let's navigate the thrilling twists and turns of the marketing world{" "}
-            <span className="text-secondary">together</span>!
-          </p>
-        </div>
+            <p>
+              Welcome to the <span className="text-secondary">T&T family</span>,
+              where marketing becomes a thrilling adventure. We are a bunch of
+              marketing geeks, fueled by the fire of our passion and a knack for
+              creativity. {" "}
+              <span className="text-secondary">
+                We don't just do our jobs, we live them.
+              </span>
+            </p>
+            <p>
+              Here at <span className="text-secondary">T&T</span>, we strive to
+              truly "get" your business. We dig deep, getting to grips with your
+              goals, your dreams, and even your worries.{" "}
+              <span className="text-secondary">
+                We're not just pushing out services, we're creating custom
+                solutions that mirror what your brand is all about.
+              </span>
+            </p>
+            <p>
+              Our bond with clients? Well, that's special. It's{" "}
+              <span className="text-secondary">
+                more than just a business deal
+              </span>
+              {" "}to us. We care for these relationships just as we do for our own
+              work - with heaps of dedication and warmth. We cheer when you
+              succeed and stand by your side when things get a bit rocky, {" "}
+              <span className="text-secondary">
+                always ready with a helping hand and a winning mindset
+              </span>
+              .
+            </p>
+            <p>
+              So, step in to a world of {" "}
+              <span className="text-secondary">T&T agency</span>, a place where
+              work meets passion, and {" "}
+              <span className="text-secondary">clients become friends</span>.
+              Let's navigate the thrilling twists and turns of the marketing world{" "}
+              <span className="text-secondary">together</span>!
+            </p>
+          </div>
         </div>
         {/* <div>
           <Social />
         </div> */}
 
         {/* Carousel  */}
-      <div className={styles.carouselContainer}>
-        <p className={styles.heading}>
-          Previous Works
-        </p>
-        <Carousel/>
-      </div>
+        <div className={styles.carouselContainer}>
+          <p className={styles.heading}>
+            Previous Works
+          </p>
+          <Carousel />
+        </div>
         {/* <div>
           <Clients />
         </div> */}
         <div className={styles.hear}>
-          <Hear/>
+          <Hear />
         </div>
       </main>
     </>
