30a3c51259eaff85e541c4504830e13788fcff83
Inaam Ur Rehman <inaamurrehman343@gmail.com>
2024-06-15 07:58:11 +0500

    Added tag

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,22 @@ import {GoogleTagManager} from "@next/third-parties/google"
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head/>
+      <Head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JZZNCJZKSS"></script>
+<script
+dangerouslySetInnerHTML={{
+  __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JZZNCJZKSS');
+  `
+}}
+/>
+        </Head>
       <body>
-        <GoogleTagManager gtmId='G-JZZNCJZKSS'/>
+       
         <Main />
         <NextScript />
       </body>
